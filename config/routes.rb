92eda6acd43e59a92aa6fd982907caf44a8bdf66
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-04-08 20:54:04 -0300

    ubicacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'page#index'
+  get 'page/cabanas'
+  get 'page/ubicacion'
+  get 'page/contacto'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
