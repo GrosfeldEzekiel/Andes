2d2b7db4fc569953bbd76cbaac333fc4d89e7f83
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-04-13 15:40:42 -0300

    fotos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+  var slideIndex = 0; 
+
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 2 seconds
+  }
