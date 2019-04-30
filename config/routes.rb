4cdf77b0bd61442ac579ecb9623207b23f670dbc
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-04-29 22:17:11 -0300

    sitemap

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get '/cabanas' => 'page#cabanas'
   get '/ubicacion' => 'page#ubicacion'
   get '/contacto' => 'page#contacto'
+  resources :sitemap, :only => :show
+  get "sitemap" => "sitemap#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
